d043abf6cd06f0039d768751d7092edfe1430559
汪汪汪 <1209497940@qq.com>
2021-06-07 15:23:19 +0800

    更新项目基本框架

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# frontend
+# dbfrontend
 
 ## Project setup
 ```
@@ -15,14 +15,5 @@ npm run serve
 npm run build
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
-
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
-
-## Instroduction
-
-This is the front end of DB.
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+//+引入组件库及相关样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+//让Vue使用ElementUI
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
