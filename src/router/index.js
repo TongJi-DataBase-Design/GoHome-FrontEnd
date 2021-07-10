3f95f4b45bfab77c3ab20e542c935832fce34a62
汪汪汪 <1209497940@qq.com>
2021-07-10 13:33:38 +0800

    解决了index中的冲突

--- src/router/index.js
+++ src/router/index.js
@@ -23,31 +23,28 @@ const routes = [
   {
     path:'/register',
     name:'Register',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+    component: () => import('../views/Register.vue')
   },
   {
     path:'/favoritePage',
     name:'favoritePage',
-    component: () => import(/* webpackChunkName: "about" */ '../views/favoritePage.vue')
+    component: () => import( '../views/favoritesPage.vue')
   },
   {
     path:'/oneFavPage',
     name:'oneFavPage',
-    component: () => import(/* webpackChunkName: "about" */ '../views/oneFavPage.vue')
+    component: () => import( '../views/oneFavPage.vue')
   },
 
   {
     path:'/historyDrawer',
     name:'historyDrawer',
-    component: () => import(/* webpackChunkName: "about" */ '../components/historyDrawer.vue')
+    component: () => import( '../components/historyDrawer.vue')
   },
   {
     path:'/license',
     name:'License',
     component:()=>import('../views/License.vue')
-  
-
-  
   },
 ]
 
