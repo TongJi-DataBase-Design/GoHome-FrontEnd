77370f83532b582661ffbdd7372329f2e0d3a257
汪汪汪 <1209497940@qq.com>
2021-07-12 19:05:01 +0800

    顾客找回密码api可用

--- src/api/customer.js
+++ src/api/customer.js
@@ -73,3 +73,16 @@ export function testToken() {
   })
 }
 
+export function changeCustomerPassword(data){
+  /*
+  修改顾客账号密码
+  返回修改状态
+  */
+  let param=new URLSearchParams(data)
+
+  return request({
+    url:'/customer/changepassword',
+    method:'post',
+    data:param
+  })
+}
