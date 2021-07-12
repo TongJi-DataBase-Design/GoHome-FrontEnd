77315bfe3d3d4e33b083cda8993bae535cd9e53a
汪汪汪 <1209497940@qq.com>
2021-07-12 20:38:58 +0800

    制作了移动的云控件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
-<!--
- * @Author: your name
- * @Date: 2021-07-05 19:12:01
- * @LastEditTime: 2021-07-12 15:22:02
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \Project-Front-End\src\views\Home.vue
--->
 <template>
-  
+  <div>
+    <movingCloud/>
+  </div>
 </template>
 
-
+<script>
+  import movingCloud from '@/components/movingCloud.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      movingCloud
+    }
+  }
+  </script>
+  
