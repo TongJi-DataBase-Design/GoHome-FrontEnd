4f7d2485f906ec2c1caae1443fde0ed3db019d26
llleoy <1421877537@qq.com>
2021-07-12 10:01:27 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -11,14 +11,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/staysView',
+    name:'staysView',
+    component: staysView
   },
-
   //注册账号路由
   {
     path:'/register',
@@ -36,22 +32,6 @@ const routes = [
     path:'/forgetPassword',
     name:'forgetPassword',
     component: () => import('../views/ForgetPassword.vue')
-  },
-  {
-    path:'/favoritePage',
-    name:'favoritePage',
-    component: () => import( '../views/favoritesPage.vue')
-  },
-  {
-    path:'/oneFavPage',
-    name:'oneFavPage',
-    component: () => import( '../views/oneFavPage.vue')
-  },
-
-  {
-    path:'/historyDrawer',
-    name:'historyDrawer',
-    component: () => import( '../components/historyDrawer.vue')
   },
   {
     path:'/license',
