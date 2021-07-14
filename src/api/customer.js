9224921755fbb853996d46d34701367c9b83af96
xuwangzilu <501661611@qq.com>
2021-07-14 14:58:41 +0800

    update files

---
+++ src/api/customer.js
@@ -0,0 +1,88 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-05 19:12:01
+ * @LastEditTime: 2021-07-09 19:38:03
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Project-Front-End\src\api\customer.js
+ */
+
+/*
+和顾客有关的api接口
+*/
+
+import request from '@/utils/request'
+
+export function customerLogin(data) {
+  /*
+  顾客登录
+  */
+
+  let param = new URLSearchParams(data)
+
+  return request({
+    url: '/login/customer',
+    method: 'post',
+    data:param
+  })
+}
+
+export function customerRegister(data){
+  /*
+  顾客注册账号
+  */
+ let param=new URLSearchParams(data)
+
+ return request({
+   url:'/register/customer',
+   method:'post',
+   data:param
+ })
+}
+
+
+
+export function customerPhoneUnique(data){
+  /*
+  检验顾客手机号是否被注册过
+  未被注册过，则返回true
+  */
+  let param=new URLSearchParams(data)
+
+  return request({
+    url:'/customer/phone',
+    method:'post',
+    data:param
+  })
+}
+
+// export function getFavorite() {
+//   return request({
+//     url: '/CustomerFavorite',
+//     method: 'get',
+//   })
+// }
+
+export function testToken() {
+  /*
+  返回当前登录的用户的创建时间
+  */
+  return request({
+    url: '/customer/createtime',
+    method: 'get'
+  })
+}
+
+export function changeCustomerPassword(data){
+  /*
+  修改顾客账号密码
+  返回修改状态
+  */
+  let param=new URLSearchParams(data)
+
+  return request({
+    url:'/customer/changepassword',
+    method:'post',
+    data:param
+  })
+}
---
+++ src/api/host.js
@@ -0,0 +1,63 @@
+/*
+和房东有关的api接口
+*/
+
+import request from '@/utils/request'
+
+export function hostLogin(data) {
+    /*
+    房东登录
+    */
+  
+    let param = new URLSearchParams(data)
+  
+    return request({
+      url: '/login/host',
+      method: 'post',
+      data:param
+    })
+  }
+
+  
+export function hostRegister(data){
+  /*
+  房东注册账号
+  */
+ let param=new URLSearchParams(data)
+
+ return request({
+   url:'/register/host',
+   method:'post',
+   data:param
+ })
+}
+
+
+
+export function hostPhoneUnique(data){
+  /*
+  检验房东手机号是否被注册过
+  未被注册过，则返回true
+  */
+  let param=new URLSearchParams(data)
+
+  return request({
+    url:'/host/phone',
+    method:'post',
+    data:param
+  })
+}
+
+export function changeHostPassword(data){
+  /*
+  修改房东账号密码
+  返回修改状态
+  */
+  let param=new URLSearchParams(data)
+
+  return request({
+    url:'/host/changepassword',
+    method:'post',
+    data:param
+  })
+}
