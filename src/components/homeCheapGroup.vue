c1116cfe8d9ac34abced032ef60378b15d7e8d5b
汪汪汪 <1209497940@qq.com>
2021-07-15 20:41:29 +0800

    修复了支付bug

--- src/components/homeCheapGroup.vue
+++ src/components/homeCheapGroup.vue
@@ -12,10 +12,10 @@
     <div v-show="this.stayList.length" class="container">
 
       <el-col style="margin-left:10%" class="col-card">
-                <cardGroup6 v-for="item in this.stayList" 
-                  :info="item" 
+                <cardGroup6 v-for="(item,index) in this.stayList" 
+                  v-bind:info="item" 
                   :offset="2"
-                  :key="item.stayID"
+                  :key="index"
                   ></cardGroup6>
 
       </el-col>
